494fee1403053dcddeb7388792028555051d3a49
Vijayk23 <vijaysachinvij23@gmail.com>
2023-08-13 13:48:16 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+function Contact() {
+  return (
+    <>
+      <div>contact</div>
+    </>
+  );
+}
